855770627569f09ae700b384b1e5ab2561e4e663
athira <athira@qburst.com>
2019-10-10 12:46:28 +0530

    Changes

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
-function Stopwatch() {
-    let sec=0, min=0, hrs=0, countStart=0,countStop=0, time, duration;
-    this.start = function() {
-        if(countStart == 1) {
+var timer = document.getElementById("timer");
+var toggleBtn = document.getElementById("toggle");
+var resetBtn = document.getElementById("reset");
+
+
+resetBtn.addEventListener("click",function(){
+  watch.reset();
+});
+
+class Stopwatch {
+    constructor() {
+        this.sec = 0;
+        this.min = 0;
+        this.hrs = 0;
+        this.countStart = 0;
+        this.countStop = 0;
+        this.time = 0;
+        this.duration = 0;
+    }
+    this.prototype.start() {
+        if(this.countStart == 1) {
             throw new Error("Stopwatch already started");
         }
-        countStart++;
-        countStop = 0;
-        time = setInterval(this.incrementTime,1000);
-    };
-    this.incrementTime = function() {
-        sec++;
-        if (sec >= 60){
-            sec=0;
-            min++;
+        this.countStart++;
+        this.countStop = 0;
+        this.time = setInterval(this.incrementTime,1000);
+    }
+    incrementTime() {
+        this.sec++;
+        if (this.sec >= 60){
+            this.sec=0;
+            this.min++;
         }
-        if(min >= 60) {
-            min=0;
-            hrs++;
+        if(this.min >= 60) {
+            this.min=0;
+            this.hrs++;
         }
-    };
-    this.stop = function() {
+        displayTime();
+    }
+    stop() {
         if(countStop == 1)
             throw new Error("Stopwatch already stopped");
         if(countStart == 0)
@@ -27,8 +45,8 @@ function Stopwatch() {
         countStop++;
         countStart = 0;
         clearInterval(time);
-    };
-    this.reset = function() {
+    }
+    reset() {
         if(countStop == 0)
             this.stop();
         sec = 0;
@@ -37,16 +55,28 @@ function Stopwatch() {
         countStart = 0;
         countStop = 0;
         duration = 0;
-    };
-    this.displayTime = function() {
-        console.log("" + hrs + " : "+ min + " : " + sec + "");
-    };
-    Object.defineProperty(this, 'duration', {
-        get: function() {
-            duration = (hrs * 3600) + (min * 60) + sec;
-            return duration;
-        }
-    });
+    }
+    displayTime() {
+        console.log("" + this.hrs + " : "+ this.min + " : " + this.sec + "");
+    }
+    
+    // Object.defineProperty(this, 'duration', {
+    //     get: function() {
+    //         duration = (hrs * 3600) + (min * 60) + sec;
+    //         return duration;
+    //     }
+    // });
 }
+// Stopwatch.start();
+var watch = new Stopwatch();
+watch.start();
 
-const sw = new Stopwatch();
+// toggleBtn.addEventListener("click",function () {
+//   if(watch.isOn){
+//     watch.stop();
+//     toggleBtn.textContent = "Start";
+//   }else {
+//     watch.start();
+//     toggleBtn.textContent = "Stop";
+//   }
+// });
